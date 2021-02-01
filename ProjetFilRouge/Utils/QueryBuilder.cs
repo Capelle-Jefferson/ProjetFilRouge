dfd1ee9b6d16956c566c72c12c8ef472898cc848
Capelle Jefferson <jefferson.capelle@sfr.fr>
2021-02-01 09:56:40 +0100

    Ajout de query builder, connectionSql

---
+++ ProjetFilRouge/Utils/QueryBuilder.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetFilRouge.Utils
+{
+    public class QueryBuilder
+    {
+        public StringBuilder request = new StringBuilder();
+        public QueryBuilder() { }
+
+        internal QueryBuilder Select(params string[] values)
+        {
+            request.Clear();
+            request.Append("SELECT ");
+            if (values.Length > 0)
+            {
+                foreach (string value in values)
+                {
+                    request.Append($"{value},");
+                }
+                request.Remove((request.Length - 1), 1);
+            }
+            else
+            {
+                request.Append("*");
+            }
+            return this;
+        }
+
+        internal QueryBuilder Insert(string table)
+        {
+            request.Clear();
+            request.Append($"INSERT INTO {table}");
+            return this;
+        }
+        
+        internal string Values(Dictionary<string, dynamic> obj)
+        {
+            request.Append("(");
+            foreach (var key in obj.Keys)
+            {
+                request.Append($"{key},");
+            }
+            request.Remove((request.Length - 1), 1);
+            request.Append(") VALUES (");
+            foreach (var key in obj.Keys)
+            {
+                if (obj[key] is string)
+                {
+                    request.Append($"'{obj[key]}',");
+                }
+                else if (obj[key] is int || obj[key] is bool)
+                {
+                    request.Append($"{obj[key]},");
+                }
+                else if (obj[key] is double)
+                {
+                    string val = obj[key].ToString();
+                    request.Append($"{val.Replace(',', '.')},");
+                }
+                else
+                {
+                    request.Append($"'{obj[key]}',");
+                }
+            }
+            request.Remove((request.Length - 1), 1);
+            request.Append(");");
+            return request.ToString();
+        }
+
+        internal string Delete(string table, long id, string idName = "id")
+        {
+            request.Clear();
+            return request.Append($"DELETE FROM {table} where {idName} = {id}").ToString();
+        }
+
+        internal string Get()
+        {
+            return request.ToString();
+        }
+
+        internal QueryBuilder Where(string key, dynamic value, string type = "=")
+        {
+            request.Append($"WHERE {key} {type} {value}");
+            return this;
+        }
+
+        internal QueryBuilder From(string table)
+        {
+            request.Append($" FROM {table} ");
+            return this;
+        }
+
+        internal QueryBuilder Update(string table)
+        {
+            request.Clear();
+            request.Append($"UPDATE {table} ");
+            return this;
+        }
+
+        internal QueryBuilder Set(Dictionary<string, dynamic> obj)
+        {
+            request.Append("SET ");
+            foreach (var key in obj.Keys)
+            {
+                if (obj[key] is string)
+                {
+                    request.Append($"{key} = '{obj[key]}',");
+                }
+                else if (obj[key] is int || obj[key] is bool)
+                {
+                    request.Append($"{key} = {obj[key]},");
+                }
+                else if (obj[key] is double)
+                {
+                    string val = obj[key].ToString();
+                    request.Append($"{key} = {val.Replace(',', '.')},");
+                }
+                else
+                {
+                    request.Append($"{key} = '{obj[key]}',");
+                }
+
+            }
+            request.Remove((request.Length - 1), 1);
+            request.Append(" ");
+            return this;
+        }
+    }
+}
