33a2d73bd928e672cbd9e352e1a6997e520c7661
Capelle Jefferson <jefferson.capelle@sfr.fr>
2021-02-18 09:53:17 +0100

    Correction d'erreur

--- ProjetFilRougeFront/src/app/_components/form-authentification/form-authentification.component.ts
+++ ProjetFilRougeFront/src/app/_components/form-authentification/form-authentification.component.ts
@@ -12,14 +12,13 @@ import { UserService } from 'src/app/_services/user.service';
 })
 export class FormAuthentificationComponent implements OnInit {
 
-  userAuth : FormGroup;
-  user : User = undefined;
-  users: User[] = [];
+  userAuth: FormGroup;
+  user: User = undefined;
 
   constructor(
     private builder: FormBuilder,
     private service: UserService
-  ) { 
+  ) {
     this.userAuth = this.builder.group({
       username: [""],
       password: [""]
@@ -30,16 +29,23 @@ export class FormAuthentificationComponent implements OnInit {
   }
 
   async onSubmit() {
-    await this.service.getAuthentification(this.userAuth.value).then((user)=> this.user = user);
+    try {
+      await this.service.getAuthentification(this.userAuth.value).then((user) => this.user = user);
 
-    // Si utilisateur reconnus
-    if(this.user != undefined){
-      sessionStorage.setItem("user", JSON.stringify(this.user));
-      location.reload();
-    }else{
-      alert("Identifiant non valide")
+      // Si utilisateur reconnus
+      if (!this.user['status']) {
+        sessionStorage.setItem("user", JSON.stringify(this.user));
+        location.reload();
+
+      // Si utilisateur non reconnus
+      } else {
+        this.userAuth.getError("Identifiant non valide");
+        alert("Identifiant non valide");
+      }
+
+    } catch {
+      this.userAuth.getError("Erreur de connexion");
+      alert("Erreur de connexion");
     }
-    
   }
-
 }
