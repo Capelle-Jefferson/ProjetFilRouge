6eae4ac8fb1b6928c4ceb326400cf42174327cc9
Segolene Demolin <demolin.segolene@gmail.com>
2021-03-02 10:03:52 +0100

    passage quiz almost done

--- ProjetFilRougeFront/src/app/_pages/passage-quiz/passage-quiz.component.ts
+++ ProjetFilRougeFront/src/app/_pages/passage-quiz/passage-quiz.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Answers } from 'src/app/_models/Answers';
 import { Question } from 'src/app/_models/question';
 import { Quizz } from 'src/app/_models/quizz';
+import { QuizzQuestionService } from 'src/app/_services/quizz-question.service';
 import { QuizzService } from 'src/app/_services/quizz.service';
 import { QuestionsComponent } from '../questions/questions.component';
 
@@ -13,41 +15,55 @@ export class PassageQuizComponent implements OnInit {
 
   @Input() quizz;
   istypeAnswer:boolean;
+  isQcmUnique:boolean;
   Next :boolean = false;
   question:any;
   nombre:number=0;
   isFinished:boolean=false;
+  answerCandidate:string;
+  lastPage:boolean=false;
   
 
-
-  constructor(private service:QuizzService) { }
+  constructor(private service:QuizzQuestionService) { }
 
   ngOnInit(): void {
-    this.quizz.questions.forEach(question=>{
-      this.istypeAnswer=question.answer.typeanswer!="Text";
-    })
+    this.suppresionLocalQuizz();
   }
 
-  addCandidateAnswer(){}
   onSubmit(){
-    console.log("Ok");
-    let nombre2=this.nombre+1;
-    this.question=this.quizz.questions[nombre2];
+    this.service.addCandidateAnswer(this.quizz.idQuizz,this.question.idQuestion,this.answerCandidate);
+    this.nombre=this.nombre+1;
+    this.question=this.quizz.questions[this.nombre];
     this.Next=true;
-    if(nombre2===this.quizz.questions.length-1){
+    this.istypeAnswer=this.question.answer.typeanswer!="Text";
+    if(this.nombre===this.quizz.questions.length-1){
       this.isFinished=true;
     }
-    //document.location.reload();
+  }
+
+  SaveAnswer(value:string){
+    this.answerCandidate=value;
+  }
+  SaveAnswerText=()=>{
+    this.answerCandidate=(<HTMLInputElement>document.getElementById("reponse")).value;
   }
   goToFirstQuestion(){
     this.question=this.quizz.questions[this.nombre];
     this.Next=true;
+    this.istypeAnswer=this.question.answer.typeAnswer!="Text";
+    this.isQcmUnique=this.question.answer.typeAnswer!="qcm_multiple";
   }
   FinduQuizz(){
-    this.suppresionLocalQuizz();
+    this.service.addCandidateAnswer(this.quizz.idQuizz,this.question.idQuestion,this.answerCandidate);
+    this.Next=false;
+    this.isFinished=false;
+    this.lastPage=true;
+    
+
   }
   // Suppresion de la variable local Quizz unt fois le quizz completé
   suppresionLocalQuizz(){
     localStorage.removeItem("quizz");
   }
+  
 }
