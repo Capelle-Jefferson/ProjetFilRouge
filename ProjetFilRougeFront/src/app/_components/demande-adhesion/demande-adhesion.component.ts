a9eaeeb6a9809859eac9ec5283cd9224dfeafd1b
Segolene Demolin <demolin.segolene@gmail.com>
2021-03-15 09:44:21 +0100

    Fix faille securite pour demande adhesion

--- ProjetFilRougeFront/src/app/_components/demande-adhesion/demande-adhesion.component.ts
+++ ProjetFilRougeFront/src/app/_components/demande-adhesion/demande-adhesion.component.ts
@@ -13,44 +13,16 @@ import { environment } from 'src/environments/environment';
 })
 export class DemandeAdhesionComponent implements OnInit {
 
-  sendForm: FormGroup;
+  
 
-  constructor(private builder : FormBuilder,
-    private router : Router,
-    private toastr : ToastrService,
-    private serviceEmail : EmailService,
-    private userservice:UserService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.sendForm = this.builder.group({
-      email: [`${environment.emailAdmin}`, Validators.email],
-      subject: ["Demande adhésion", Validators.required],
-      message: [
-        "Bonjour " + ",\n\n" +
-        "Je souhaite créer un compte pour l'application Projet Fil rouge\n\n"+
-        "Voici les informations nécessaires:\n"+
-        "Nom: <Votre nom> \n"+
-        "Prenom: <Votre prénom>\n"+
-        "Email: <Votre email>\n" + 
-        `\n\n\n`+
-        "Bien cordialement,\n\n",
-        Validators.required
-      ]
-    })
+    
   }
 
   
-  async onSubmit(){
-    let res : Boolean
-    await this.serviceEmail.sendEmail(this.sendForm.value).then(data => res = data)
   
-    if(res){
-      this.toastr.success("L'email à bien été envoyé");
-      location.replace("");
-    }else{
-      this.toastr.error("L'email n'a pas était envoyé");
-    }
-  }
 }
 
 
--- ProjetFilRougeFront/src/app/_pages/authentification/authentification.component.ts
+++ ProjetFilRougeFront/src/app/_pages/authentification/authentification.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { EmailService } from 'src/app/_services/email.service';
+import { UserService } from 'src/app/_services/user.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-authentification',
@@ -8,13 +13,52 @@ import { Router } from '@angular/router';
 })
 export class AuthentificationComponent implements OnInit {
 
+  sendForm: FormGroup;
 
-  constructor(
-
+  constructor(private builder : FormBuilder,
+    private router : Router,
+    private toastr : ToastrService,
+    private serviceEmail : EmailService,
+    private userservice:UserService
   ) { }
 
   ngOnInit(): void {
+    this.sendForm = this.builder.group({
+      email: [`${environment.emailAdmin}`, Validators.email],
+      subject: ["Demande adhésion", Validators.required],
+      message: [
+        "Bonjour " + ",\n\n" +
+        "Je souhaite créer un compte pour l'application Projet Fil rouge\n\n"+
+        "Voici les informations nécessaires:\n"+
+        "Nom: <Votre nom> \n"+
+        "Prénom: <Votre prénom>\n"+
+        "Email: <Votre email>\n" + 
+        `\n\n`+
+        "Bien cordialement,\n\n",
+        Validators.required
+      ]
+    })
 
   }
 
+  displayForm(id){
+    var div = document.getElementById(id);
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+
+  async onSubmit(){
+    let res : Boolean
+    await this.serviceEmail.sendEmail(this.sendForm.value).then(data => res = data)
+  
+    if(res){
+      this.toastr.success("L'email à bien été envoyé");
+      location.replace("");
+    }else{
+      this.toastr.error("L'email n'a pas était envoyé");
+    }
+  }
 }
